93c6cbdc1790a83beb1adc6355399768b1445013
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-16 23:20:53 +0000

    header file containing all prototypes and macros

--- main.h
+++ main.h
@@ -29,17 +29,17 @@ int (*fn)(va_list, char[], int, int, int, int);
 * typedef struct fmt fmt_t - Struct op
 *
 * @fmt: The format.
-* @fm_t: function.
+* @fm_t: The function associated.
 */
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list v_list, char input[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, va_list list, char input[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
-/* Funtions to print chars and strings */
+/* Funtions that print chars and strings */
 int print_char(va_list types, char input[], int flags, int width, int precision, int size);
 int print_string(va_list types, char input[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char input[], int flags, int width, int precision, int size);
-/* Functions to print numbers */
+/* Functions that print numbers */
 int print_int(va_list types, char input[], int flags, int width, int precision, int size);
 int print_binary(va_list types, char input[], int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char input[], int flags, int width, int precision, int size);
@@ -53,8 +53,8 @@ int print_non_printable(va_list types, char input[], int flags, int width, int p
 int print_pointer(va_list types, char input[], int flags, int width, int precision, int size);
 /* Functions that handle other specifiers */
 int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list v_list);
-int get_precision(const char *format, int *i, va_list v_list);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 /*Function that prints string in reverse*/
 int print_reverse(va_list types, char input[], int flags, int width, int precision, int size);
