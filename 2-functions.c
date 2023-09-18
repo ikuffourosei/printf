c7e8a9b4b8a8b4ff98757b08839ad143e5db512d
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2023-09-18 18:14:38 +0000

    functions that print unsigned decimal, octal and upper and lower hexa decimal

--- 2-functions.c
+++ 2-functions.c
@@ -12,84 +12,83 @@ int print_hexa(va_list types, char map_to[], char input[], int flags, char flag_
 
 int print_unsigned(va_list types, char input[], int flags, int width, int precision, int size)
 {
-    int i = BUFF_SIZE - 2;
-    unsigned long int num = va_arg(types, unsigned long int);
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
 
-    num = convert_size_unsgnd(num, size);
-    if (num == 0)
-    {
-        input[i--] = '0';
-        input[BUFF_SIZE - 1] = '\0';
-        while (num > 0)
-        {
-            input[i--] = (num % 10) + '0';
-            num /= 10;
-        }
-        i++;
-    }
-    return (write_unsgnd(0, i, input, flags, width, precision, size));
+	num = convert_size_unsgnd(num, size);
+	if (num == 0)
+	{
+		input[i--] = '0';
+		input[BUFF_SIZE - 1] = '\0';
+		while (num > 0)
+		{
+			input[i--] = (num % 10) + '0';
+			num /= 10;
+		}
+		i++;
+	}
+	return (write_unsgnd(0, i, input, flags, width, precision, size));
 }
 
 int print_octal(va_list types, char input[], int flags, int width, int precision, int size)
 {
-    int i = BUFF_SIZE - 2;
-    unsigned long int num = va_arg(types, unsigned long int);
-    unsigned long int init_num = num;
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
 
-    UNUSED(width);
-    num = convert_size_unsgnd(num, size);
-    if (num == 0)
-    {
-        input[i--] = '0';
-    }
-    input[BUFF_SIZE - 1] = '\0';
-    while (num > 0)
-    {
-        input[i--] = (num % 8) + '0';
-        num /= 8;
-    }
-    if (flags & F_HASH && init_num != 0)
-    {
-        input[i--] = '0';
-    }
-    i++;
-    return (write_unsgnd(0, i, input, flags, width, precision, size));
+	UNUSED(width);
+	num = convert_size_unsgnd(num, size);
+	if (num == 0)
+	{
+		input[i--] = '0';
+	}
+	input[BUFF_SIZE - 1] = '\0';
+	while (num > 0)
+	{
+		input[i--] = (num % 8) + '0';
+		num /= 8;
+	}
+	if (flags & F_HASH && init_num != 0)
+	{
+		input[i--] = '0';
+	}
+	i++;
+	return (write_unsgnd(0, i, input, flags, width, precision, size));
 }
 
 int print_hexadecimal(va_list types, char input[], int flags, int width, int precision, int size)
 {
-    return (print_hexa(types, "0123456789abcdef", input, flags, 'x', width, precision, size));
+	return (print_hexa(types, "0123456789abcdef", input, flags, 'x', width, precision, size));
 }
 
 int print_hexa_upper(va_list types, char input[], int flags, int width, int precision, int size)
 {
-    return (print_hexa(types, "0123456789ABCDEF", input, flags, 'X', width, precision, size));
+	return (print_hexa(types, "0123456789ABCDEF", input, flags, 'X', width, precision, size));
 }
 
 int print_hexa(va_list types, char map_to[], char input[], int flags, char flag_ch, int width, int precision, int size)
 {
-    int i = BUFF_SIZE - 2;
-    unsigned long int num = va_arg(types, unsigned long int);
-    unsigned long int init_num = num;
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
 
-    UNUSED(width);
-    num = convert_size_unsgnd(num, size);
-    if (num == 0)
-    {
-        input[i--] = '0';
-    }
-    input[BUFF_SIZE - 1] = '\0';
-    while (num > 0)
-    {
-        input[i--] = map_to[num % 16];
-        num /= 16;
-    }
-    if (flags & F_HASH && init_num != 0)
-    {
-        input[i--] = flag_ch;
-        input[i--] = '0';
-    }
-    i++;
-    return (write_unsgnd(0, i, input, flags, width, precision, size));
+	UNUSED(width);
+	num = convert_size_unsgnd(num, size);
+	if (num == 0)
+	{
+		input[i--] = '0';
+	}
+	input[BUFF_SIZE - 1] = '\0';
+	while (num > 0)
+	{
+		input[i--] = map_to[num % 16];
+		num /= 16;
+	}
+	if (flags & F_HASH && init_num != 0)
+	{
+		input[i--] = flag_ch;
+		input[i--] = '0';
+	}
+	i++;
+	return (write_unsgnd(0, i, input, flags, width, precision, size));
 }
-
